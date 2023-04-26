3690367a1f1c1326990ab14f00bd78ab9159289f
Hobr <mail@hobr.site>
2023-04-26 14:30:45 +0800

    透明配置

--- lua/plugin/ui/transparent.lua
+++ lua/plugin/ui/transparent.lua
@@ -2,4 +2,13 @@ local M = {
   "xiyaowong/transparent.nvim",
 }
 
+M.config = function()
+  require("transparent").setup({
+    extra_groups = {
+      "NormalFloat",
+      "NvimTreeNormal",
+    },
+  })
+end
+
 return M
