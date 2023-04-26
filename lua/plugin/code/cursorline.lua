641ee98885c633063330948d5fe2b1063c68c466
Hobr <mail@hobr.site>
2023-04-26 22:44:35 +0800

    cursorline配置

--- lua/plugin/code/cursorline.lua
+++ lua/plugin/code/cursorline.lua
@@ -2,4 +2,19 @@ local M = {
   "yamatsum/nvim-cursorline",
 }
 
+M.config = function()
+  require("nvim-cursorline").setup {
+    cursorline = {
+      enable = true,
+      timeout = 1000,
+      number = false,
+    },
+    cursorword = {
+      enable = true,
+      min_length = 3,
+      hl = { underline = true },
+    },
+  }
+end
+
 return M
