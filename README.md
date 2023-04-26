04d2c6732d9af784b7eed4198d0ea95370ea4574
Hobr <mail@hobr.site>
2023-04-26 21:25:32 +0800

    nvim-treesitter配置

--- README.md
+++ README.md
@@ -58,7 +58,7 @@ rm -rf ~/.config/nvim ~/.local/share/nvim ~/.local/state/nvim ~/.cache/nvim
 - [x] [nvim-cmp](https://github.com/hrsh7th/nvim-cmp) 代码补全
 - [x] [cmp-under-comparator](https://github.com/lukas-reineke/cmp-under-comparator) cmp扩展，优化显示顺序
 - [x] [cmp-nvim-lsp](https://github.com/hrsh7th/cmp-nvim-lsp) cmp扩展，接入LSP
-- [ ] [nvim-treesitter](https://github.com/nvim-treesitter/nvim-treesitter) 代码高亮
+- [x] [nvim-treesitter](https://github.com/nvim-treesitter/nvim-treesitter) 代码高亮
 - [ ] [LuaSnip](https://github.com/L3MON4D3/LuaSnip) 代码片段
 - [ ] [friendly-snippets](https://github.com/rafamadriz/friendly-snippets) 预设代码片段
 - [ ] [nvim-ufo](https://github.com/kevinhwang91/nvim-ufo) 代码折叠
--- lua/plugin/code/treesitter.lua
+++ lua/plugin/code/treesitter.lua
@@ -5,4 +5,80 @@ local M = {
   end,
 }
 
+M.config = function()
+  require("nvim-treesitter.configs").setup {
+    ensure_installed = {
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "bash",
+      "bibtex",
+      "c",
+      "cmake",
+      "cpp",
+      "css",
+      "diff",
+      "dockerfile",
+      "git_config",
+      "gitattributes",
+      "gitcommit",
+      "gitignore",
+      "go",
+      "graphql",
+      "hlsl",
+      "html",
+      "http",
+      "ini",
+      "json",
+      "latex",
+      "llvm",
+      "make",
+      "matlab",
+      "markdown",
+      "php",
+      "python",
+      "ruby",
+      "rust",
+      "scheme",
+      "scss",
+      "sql",
+      "typescript",
+      "verilog",
+      "vim",
+      "vimdoc",
+      "vue",
+      "wgsl",
+      "yaml",
+    },
+    sync_install = true,
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
+      additional_vim_regex_highlighting = false,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "gnn",
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
+      },
+    },
+    indent = {
+      enable = true,
+    },
+  }
+end
+
 return M
