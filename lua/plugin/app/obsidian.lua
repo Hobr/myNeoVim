95712f4e06b12e8d3660b3c18f335db04355f68c
Hobr <mail@hobr.site>
2023-05-05 14:24:16 +0800

    obsidian配置

--- lua/plugin/app/obsidian.lua
+++ lua/plugin/app/obsidian.lua
@@ -1,11 +1,63 @@
-local M = {
+return {
   "epwalsh/obsidian.nvim",
+  lazy = true,
+  event = { "BufReadPre /mnt/e/Project/note/**.md" },
   dependencies = {
-    "nvim-telescope/telescope.nvim",
     "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
     "hrsh7th/nvim-cmp",
+    "nvim-telescope/telescope.nvim",
+    "godlygeek/tabular",
+    "preservim/vim-markdown",
   },
-}
+  opts = {
+    dir = "/mnt/e/Project/note",
+    daily_notes = {
+      folder = "日记",
+    },
+    completion = {
+      nvim_cmp = true,
+    },
+    note_id_func = function(title)
+      local suffix = ""
+      if title ~= nil then
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.time()) .. "-" .. suffix
+    end,
+    disable_frontmatter = false,
+    note_frontmatter_func = function(note)
+      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+      if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+      return out
+    end,
+    templates = {
+      subdir = "template",
+      date_format = "%Y-%m-%d-%a",
+      time_format = "%H:%M",
+    },
 
-return M
+    follow_url_func = function(url)
+      vim.fn.jobstart { "open", url }
+    end,
+    use_advanced_uri = true,
+    open_app_foreground = false,
+  },
+  config = function(_, opts)
+    require("obsidian").setup(opts)
+    vim.keymap.set("n", "gf", function()
+      if require("obsidian").util.cursor_on_markdown_link() then
+        return "<cmd>ObsidianFollowLink<CR>"
+      else
+        return "gf"
+      end
+    end, { noremap = false, expr = true })
+  end,
+}
